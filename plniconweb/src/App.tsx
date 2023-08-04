4dfae57536266017f3d8ee5630c83ed50c513e03
akmalnatha <akmalabhirama2002@gmail.com>
2023-08-04 21:39:11 +0700

    akhirnya tamat set up

--- plniconweb/src/App.tsx
+++ plniconweb/src/App.tsx
@@ -1,14 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
+      <p className='font-montserrat font-bold text-2xl text-blue-primary'>kontol</p>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -25,9 +26,9 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
+      <p className="font-montserrat font-bold">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
--- plniconweb/tailwind.config.js
+++ plniconweb/tailwind.config.js
@@ -1,11 +1,55 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        yellow: {
+          primary: "#F8DB25",
+          hover: "#FFEA6A",
+          click: "#E7B400",
+          graph: "#FFA41B",
+        },
+        blue: {
+          primary: "#09AEEF",
+          hover: "#59C6F1",
+          click: "#0B7AA6",
+          graph: "#015CBA",
+        },
+        red: {
+          primary: "#BD1B1B",
+          hover: "#E64444",
+          click: "#AA0808",
+          graph: "#BD1B1B",
+        },
+        green: {
+          primary: "#1D7331",
+          hover: "#3CCE5D",
+          click: "#004F12",
+          graph: "#1D7331",
+        },
+        purple: {
+          graph: "#9384D1",
+        },
+        pink: {
+          graph: "#F266AB",
+        },
+        neutral: {
+          500: "#fafaff",
+          100: "#eef0f2",
+          300: "#ecebe4",
+          500: "#daddd8",
+          700: "#1c1c1c",
+        },
+        text: {
+          light: "#F8F8F8",
+          dark: "#031B34",
+        },
+      },
+    },  
   },
   plugins: [],
-}
-
+};
