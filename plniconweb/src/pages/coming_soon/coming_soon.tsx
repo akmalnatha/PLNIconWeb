e31292fc1003dda9d6818684400745c5967c2cd3
Muhammad Hanif Al Faithoni <et4w4zz@gmail.com>
2023-08-25 00:10:59 +0700

    feat coming soon, fix datefield

---
+++ plniconweb/src/pages/coming_soon/coming_soon.tsx
@@ -0,0 +1,12 @@
+import cs from "../../../public/assets/coming_soon.svg";
+function ComingSoon() {
+  return (
+    <>
+      <div className="w-full h-screen">
+        <div className="w-full h-full bg-[url(./assets/coming_soon.svg)] bg-cover bg-no-repeat"></div>
+      </div>
+    </>
+  );
+}
+
+export default ComingSoon;
