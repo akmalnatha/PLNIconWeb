a3755d0bdac719fc80d2b17b201b93f4a9c660fd
akmalnatha <akmalabhirama2002@gmail.com>
2023-09-05 13:51:23 +0700

    include basename

--- plniconweb/src/main.tsx
+++ plniconweb/src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ToastContainer } from 'react-toastify'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
+  <BrowserRouter basename="/dist">
     <ToastContainer/>
     <App />
+  </BrowserRouter>
   </>,
 )
